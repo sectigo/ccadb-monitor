0eb15d6636ed515476361e14ed2eff2b524a2424
Martijn Katerbarg <martijn.katerbarg@sectigo.com>
2026-01-26 10:45:43 +0100

    Add Slack logging / alerting

--- config/services.php
+++ config/services.php
@@ -30,8 +30,8 @@ return [
 
     'slack' => [
         'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+            'bot_user_oauth_token' => env('LOG_SLACK_OATH_TOKEN'),
+            'channel' => env('LOG_SLACK_CHANNEL'),
         ],
     ],
 
